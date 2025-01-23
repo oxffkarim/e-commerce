bb0cb1c66f499bc044cabd208fad0bd6df630655
Karim Fathy <oxffkarim@gmail.com>
2025-01-24 00:16:14 +0200

    version 2

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import category,customer,product,order
+admin.site.register(category)
+admin.site.register(customer)
+admin.site.register(product)
+admin.site.register(order)
